d1adb2845df169d5840803ecde2064cd4ce9caeb
Matt Savage <matthew.savage@gmail.com>
2020-04-02 06:36:52 +0100

    typesafe: Result lens for responses

---
+++ http4k-typesafe-routing/src/test/kotlin/org/http4k/typesafe/routing/messages/resultTest.kt
@@ -0,0 +1,35 @@
+package org.http4k.typesafe.routing.messages
+
+import com.natpryce.Failure
+import com.natpryce.Success
+import org.http4k.core.Response
+import org.http4k.core.Status.Companion.BAD_REQUEST
+import org.http4k.core.Status.Companion.OK
+import org.http4k.typesafe.routing.messages.body.text
+import org.http4k.typesafe.routing.messages.body.textPlain
+import org.http4k.typesafe.routing.responses.status
+import org.junit.jupiter.api.Test
+
+@Suppress("MemberVisibilityCanBePrivate")
+internal class ResultTest {
+
+    val lens: ResultMessageLens<Response, String, String> = result(
+        status(OK) but text(),
+        status(BAD_REQUEST) but text())
+
+    @Test
+    fun `works for failure`() {
+        responseContract(
+            lens,
+            Failure("something went wrong"),
+            Response(BAD_REQUEST).textPlain("something went wrong"))
+    }
+
+    @Test
+    fun `works for success`() {
+        responseContract(
+            lens,
+            Success("hello world"),
+            Response(OK).textPlain("hello world"))
+    }
+}
